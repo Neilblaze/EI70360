c0e5213832aeeb27df330d411e17765567ed5a06
neilblaze <putubanerjee23@gmail.com>
2023-05-23 00:30:42 +0530

    embeddings [wip]

--- transformers/main.py
+++ transformers/main.py
@@ -92,6 +92,17 @@ class EncoderLayer(nn.Module):
         norm_2 = self.norm_2(pwffn, norm_1)
         return norm_2
 
+class Embeddings(nn.Module):
+
+    def __init__(self, vocab_size, padding_idx, d_model):
+        super().__init__()
+        self.d=d_model
+        self.embed = nn.Embedding(vocab_size, d_model, padding_idx=padding_idx)
+
+    def forward(self, x):
+        embedding = self.embed(x)
+        return embedding * math.sqrt(self.d_model)
+
 if __name__=="__main__":
 
     toy_encodings = torch.Tensor([
